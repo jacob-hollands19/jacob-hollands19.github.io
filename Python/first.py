5a1d33b7d21230c39097a1451374cedcbc6c9c46
jacob-hollands19 <73252388+jacob-hollands19@users.noreply.github.com>
2021-01-11 16:18:28 -0800

    Added python folder and starting python files

---
+++ Python/first.py
@@ -0,0 +1,20 @@
+# name = input("What is your name? ")
+
+# if name.lower() == "jacob":
+#     print("Welcome back Jacob!")
+# elif name.lower() == "paul": 
+#     print("Welcome abck Paul")
+# else: 
+#     print("Unauthorized User")
+
+
+
+# end = int(input("Ending Number: "))
+# def factorial(end):
+#     current = 1
+#     running_fact = 1
+#     while current <= end:
+#         running_fact *= current
+#         current +=1
+#     print(running_fact)
+# factorial(end)
---
+++ Python/fizzbuzz.py
@@ -0,0 +1,11 @@
+i = 0
+while i <= 100:
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(str(i))
+    i += 1
