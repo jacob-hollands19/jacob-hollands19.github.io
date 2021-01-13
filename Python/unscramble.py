b5d6d12a433ffadd9bdca09e8a87c39f693a5b13
jacob-hollands19 <73252388+jacob-hollands19@users.noreply.github.com>
2021-01-13 10:55:46 -0800

    Fixed and added unscramble!

---
+++ Python/unscramble.py
@@ -0,0 +1,8 @@
+str1 = input("Enter a string to unscramble: ")
+str1 = str1.lower()
+f = open("C:\\Users\Jacob\Desktop\Bootcamp\jacob-hollands19.github.io\Python\wordlist.txt", "r")
+print(set(str1))
+for x in f:
+    x = x.strip()
+    if len(str1) == len(x) and set(str1) == set(x):
+        print(x)
